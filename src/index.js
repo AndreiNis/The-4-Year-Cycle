4993737ff00cdcd453f5cbfb2a0baec272c4b90e
Andrei Nistor <andrei.nistor.p@gmail.com>
2019-08-26 12:01:36 -0400

    modernize v3 code to v5, get graph working

--- src/index.js
+++ src/index.js
@@ -2,145 +2,137 @@
 import * as d3 from 'd3';
 
 
-
-var margin = { top: 20, right: 40, bottom: 30, left: 20 },
-    width = 960 - margin.left - margin.right,
-    height = 500 - margin.top - margin.bottom,
-    barWidth = Math.floor(width / 19) - 1;
-
-var x = d3.scaleLinear()
-    .range([barWidth / 2, width - barWidth / 2]);
-
-var y = d3.scaleLinear()
-    .range([height, 0]);
-
-var yAxis = d3.axisLeft(y)
-    .tickSize(-width)
-    .tickFormat(function (d) { return Math.round(d / 1e6) + "M"; });
-
 // An SVG element with a bottom-right origin.
-const svg = d3.select('#root-svg')
-    .attr("width", width + margin.left + margin.right)
-    .attr("height", height + margin.top + margin.bottom)
-    .append("g")
+const svg = d3.select('#root-svg'),
+    margin = { top: 20, right: 20, bottom: 110, left: 40 },
+    margin2 = { top: 430, right: 20, bottom: 30, left: 40 },
+    width = +svg.attr("width") - margin.left - margin.right,
+    height = +svg.attr("height") - margin.top - margin.bottom,
+    height2 = +svg.attr("height") - margin2.top - margin2.bottom;
+
+// var parseDate = d3.timeParse("%b %Y");
+
+var x = d3.scaleTime().range([0, width]),
+    x2 = d3.scaleTime().range([0, width]),
+    y = d3.scaleLinear().range([height, 0]),
+    y2 = d3.scaleLinear().range([height2, 0]);
+
+var xAxis = d3.axisBottom(x),
+    xAxis2 = d3.axisBottom(x2),
+    yAxis = d3.axisLeft(y);
+
+var brush = d3.brushX()
+    .extent([[0, 0], [width, height2]])
+    .on("brush", brushed);
+
+var zoom = d3.zoom()
+    .scaleExtent([1, Infinity])
+    .translateExtent([[0, 0], [width, height]])
+    .extent([[0, 0], [width, height]])
+    .on("zoom", zoomed);
+
+var area = d3.area()
+    .curve(d3.curveMonotoneX)
+    .x(function (d) { return x(d.snapped_at); })
+    .y0(height)
+    .y1(function (d) { return y(d.price); });
+
+var area2 = d3.area()
+    .curve(d3.curveMonotoneX)
+    .x(function (d) { return x2(d.snapped_at); })
+    .y0(height2)
+    .y1(function (d) { return y2(d.price); });
+
+svg.append("defs").append("clipPath")
+    .attr("id", "clip")
+    .append("rect")
+    .attr("width", width)
+    .attr("height", height);
+
+var focus = svg.append("g")
+    .attr("class", "focus")
     .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
-// A sliding container to hold the bars by birthyear.
-// var birthyears = svg.append("g")
-//     .attr("class", "birthyears");
+var context = svg.append("g")
+    .attr("class", "context")
+    .attr("transform", "translate(" + margin2.left + "," + margin2.top + ")");
 
-// A label for the current year.
-var title = svg.append("text")
-    .attr("class", "title")
-    .attr("dy", ".71em")
-    .text(2000);
-
-// d3.csv('./data/btc-usd-max.csv').then(data => {
-//     data.forEach(d => {
-//         d.price = +d.price;
-//         d.market_cap = +d.market_cap;
-//         d.total_volume = +d.total_volume;
-//     });
-//     render(data);
-// });
-let parseTime = d3.timeParse("%B %d, %Y");
+// new Date().getYear() + 1900
 
 d3.csv('./data/btc-usd-max.csv').then(data => {
+    // let parseTime = d3.timeFormat("%B %d, %Y");
     data.forEach(d => {
         d.price = +d.price;
         d.market_cap = +d.market_cap;
         d.total_volume = +d.total_volume;
-        d.snapped_at = parseTime(d.snapped_at);
+        d.snapped_at = new Date(d.snapped_at);
     });
-    // });
 
-    // Compute the extent of the data set in age and years.
-    // var age1 = d3.max(data, function (d) { return d.age; }),
-    //     year0 = d3.min(data, function (d) { return d.year; }),
-    //     year1 = d3.max(data, function (d) { return d.year; }),
-    //     year = year1;
-    let formatYear = d3.timeFormat("%Y");
-
-    let yearMax = d3.max(data, function (d) {return formatYear(d.snapped_at)}),
-        yearMin = d3.min(data, function (d) {return formatYear(d.snapped_at)})
-               
-
-    // Update the scale domains.
-    x.domain([yearMin, yearMax]);
+    x.domain(d3.extent(data, function (d) { return d.snapped_at; }));
     y.domain([0, d3.max(data, function (d) { return d.price; })]);
+    x2.domain(x.domain());
+    y2.domain(y.domain());
 
-    // // Produce a map from year and birthyear to [male, female].
-    // data = d3.nest()
-    //     .key(function (d) { return d.year; })
-    //     .key(function (d) { return d.year - d.age; })
-    //     .rollup(function (v) { return v.map(function (d) { return d.people; }); })
-    //     .map(data);
+    focus.append("path")
+        .datum(data)
+        .attr("class", "area")
+        .attr("d", area);
 
-    // // Add an axis to show the population values.
-    svg.append("g")
-        .attr("class", "y axis")
-        .attr("transform", "translate(" + width + ",0)")
-        .call(yAxis)
-        .selectAll("g")
-        .filter(function (value) { return !value; })
-        .classed("zero", true);
+    focus.append("g")
+        .attr("class", "axis axis--x")
+        .attr("transform", "translate(0," + height + ")")
+        .call(xAxis);
 
-    // // Add labeled rects for each birthyear (so that no enter or exit is required).
-    var year = years.selectAll(".snapped_at")
-        .data(d3.range(yearMin, yearMax + 1, 5))
-        .enter().append("g")
-        .attr("class", "year")
-        .attr("transform", function (year) { return "translate(" + x(year) + ",0)"; });
+    focus.append("g")
+        .attr("class", "axis axis--y")
+        .call(yAxis);
 
-    birthyear.selectAll("rect")
-        .data(function (year) { return data[year] || [0, 0]; })
-        .enter().append("rect")
-        .attr("x", -barWidth / 2)
-        .attr("width", barWidth)
-        .attr("y", y)
-        .attr("height", function (value) { return height - y(value); });
+    context.append("path")
+        .datum(data)
+        .attr("class", "area")
+        .attr("d", area2);
 
-    // // Add labels to show birthyear.
-    // birthyear.append("text")
-    //     .attr("y", height - 4)
-    //     .text(function (birthyear) { return birthyear; });
+    context.append("g")
+        .attr("class", "axis axis--x")
+        .attr("transform", "translate(0," + height2 + ")")
+        .call(xAxis2);
 
-    // // Add labels to show age (separate; not animated).
-    // svg.selectAll(".age")
-    //     .data(d3.range(0, age1 + 1, 5))
-    //     .enter().append("text")
-    //     .attr("class", "age")
-    //     .attr("x", function (age) { return x(year - age); })
-    //     .attr("y", height + 4)
-    //     .attr("dy", ".71em")
-    //     .text(function (age) { return age; });
+    context.append("g")
+        .attr("class", "brush")
+        .call(brush)
+        .call(brush.move, x.range());
 
-    // // Allow the arrow keys to change the displayed year.
-    // window.focus();
-    // d3.select(window).on("keydown", function () {
-    //     switch (d3.event.keyCode) {
-    //         case 37: year = Math.max(year0, year - 10); break;
-    //         case 39: year = Math.min(year1, year + 10); break;
-    //     }
-    //     update();
-    // });
-
-    // function update() {
-    //     if (!(year in data)) return;
-    //     title.text(year);
-
-    //     birthyears.transition()
-    //         .duration(750)
-    //         .attr("transform", "translate(" + (x(year1) - x(year)) + ",0)");
-
-    //     birthyear.selectAll("rect")
-    //         .data(function (birthyear) { return data[year][birthyear] || [0, 0]; })
-    //         .transition()
-    //         .duration(750)
-    //         .attr("y", y)
-    //         .attr("height", function (value) { return height - y(value); });
-    // }
+    svg.append("rect")
+        .attr("class", "zoom")
+        .attr("width", width)
+        .attr("height", height)
+        .attr("transform", "translate(" + margin.left + "," + margin.top + ")")
+        .call(zoom);
 });
 
+function brushed() {
+    if (d3.event.sourceEvent && d3.event.sourceEvent.type === "zoom") return; // ignore brush-by-zoom
+    var s = d3.event.selection || x2.range();
+    x.domain(s.map(x2.invert, x2));
+    focus.select(".area").attr("d", area);
+    focus.select(".axis--x").call(xAxis);
+    svg.select(".zoom").call(zoom.transform, d3.zoomIdentity
+        .scale(width / (s[1] - s[0]))
+        .translate(-s[0], 0));
+}
 
+function zoomed() {
+    if (d3.event.sourceEvent && d3.event.sourceEvent.type === "brush") return; // ignore zoom-by-brush
+    var t = d3.event.transform;
+    x.domain(t.rescaleX(x2).domain());
+    focus.select(".area").attr("d", area);
+    focus.select(".axis--x").call(xAxis);
+    context.select(".brush").call(brush.move, x.range().map(t.invertX, t));
+}
+
+function type(d) {
+    d.date = d.snapped_at;
+    d.price = +d.price;
+    return d;
+}
 
